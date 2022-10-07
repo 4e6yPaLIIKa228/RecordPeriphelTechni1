e825e0ca5a1df2f58ea9551cd7c794755f52ad61
K5092022KST@outlook.com <K5092022KST@outlook.com>
2022-10-07 14:41:25 +0300

    07/10

---
+++ Connection/DBConnection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecordPeriphelTechniс.Connection
+{
+    internal class DBConnection
+    {
+        public static string myConn = $@"Data Source=YechetPerTech.db;Version=3;";
+    }
+}
